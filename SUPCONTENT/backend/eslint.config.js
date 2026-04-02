4a412894b9521fe86591f097ce35b76e431529af
sfdhafsa <hafsasifeddine8@gmail.com>
2026-04-02 21:39:00 +0200

    chore: uniforming eslint

---
+++ SUPCONTENT/backend/eslint.config.js
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import unicorn from "eslint-plugin-unicorn";
+
+export default [
+
+js.configs.recommended,
+
+{
+files:["src/**/*.js"],
+
+plugins:{
+unicorn
+},
+
+rules:{
+
+"no-unused-vars":"warn",
+
+"eqeqeq":"warn",
+
+"unicorn/filename-case":[
+"warn",
+{
+case:"kebabCase"
+}
+]
+
+}
+
+}
+
+];
