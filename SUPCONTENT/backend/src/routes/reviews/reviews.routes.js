1b30a5f88405a95d9e28dd7fe0b5d130c0a1d3ab
sfdhafsa <hafsasifeddine8@gmail.com>
2026-05-08 14:04:48 +0200

    feat: add reviwews

--- SUPCONTENT/backend/src/routes/reviews/reviews.routes.js
+++ SUPCONTENT/backend/src/routes/reviews/reviews.routes.js
@@ -1,29 +1,26 @@
 // src/routes/reviews/reviews.routes.js
 
 import express from "express";
+
 import {
   createReview,
   getReviewsByMovie,
   updateReview,
   deleteReview,
-  likeReview,
-  unlikeReview,
 } from "../../controllers/reviews/reviews.controller.js";
 
-import {protect} from "../../middlewares/auth.middleware.js";
+import {
+  protect,
+} from "../../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/", protect, createReview);
+router.post("/",protect,createReview);
 
-router.get("/movie/:tmdbId", getReviewsByMovie);
+router.get("/movie/:tmdbId",getReviewsByMovie);
 
-router.patch("/:id", protect, updateReview);
+router.patch("/:id",protect,updateReview);
 
-router.delete("/:id", protect, deleteReview);
-
-router.post("/:id/like", protect, likeReview);
-
-router.delete("/:id/like", protect, unlikeReview);
+router.delete("/:id",protect,deleteReview);
 
 export default router;
