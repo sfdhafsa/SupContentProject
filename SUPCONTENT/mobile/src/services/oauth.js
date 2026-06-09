ad0b8eef0f1052870cdb19e94091ac2674df5683
Ikram Lotfi <lotfikram2000@gmail.com>
2026-06-09 19:29:04 +0200

    feature :fixing Oauth Connexion

--- SUPCONTENT/mobile/src/services/oauth.js
+++ SUPCONTENT/mobile/src/services/oauth.js
@@ -3,6 +3,10 @@ import { Platform } from 'react-native';
 import { getGoogleOAuthUrl } from './authApi';
 
 export function getOAuthRedirectUri() {
+  if (process.env.EXPO_PUBLIC_OAUTH_REDIRECT_URI) {
+    return process.env.EXPO_PUBLIC_OAUTH_REDIRECT_URI;
+  }
+
   if (Platform.OS === 'web') {
     return `${window.location.origin}/auth/callback`;
   }
